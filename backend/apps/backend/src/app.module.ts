8a35b01a9aa17a80f8f210d6b19a34ca0cb69b61
dor133 <dore.mathieu@protonmail.com>
2023-04-11 17:33:32 +0200

    feat: add users schemas

--- backend/apps/backend/src/app.module.ts
+++ backend/apps/backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './controllers/app.controller'
 import { AppService } from './services/app.service'
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-    imports: [],
+    imports: [MongooseModule.forRoot('mongodb+srv://mdore:FjfrjcxKYfHowB0h@my-forum.48ponwu.mongodb.net/?retryWrites=true&w=majority')],
     controllers: [AppController],
     providers: [AppService],
 })
--- backend/apps/backend/src/controllers/app.controller.ts
+++ backend/apps/backend/src/controllers/app.controller.ts
@@ -5,8 +5,13 @@ import { AppService } from '../services/app.service'
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
+    // @Get()
+    // getHello(): string {
+    //     return this.appService.getHello()
+    // }
+
     @Get()
-    getHello(): string {
-        return this.appService.getHello()
+    getUsers(): string {
+        return this.appService.getUsers()
     }
 }
