df7b0f26d65f8f09c0c01724380a956a609c584f
dor133 <dore.mathieu@protonmail.com>
2023-04-13 16:44:55 +0200

    refactor: use environment var instead of writing db url

--- backend/apps/backend/src/app.module.ts
+++ backend/apps/backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './controllers/app.controller'
 import { AppService } from './services/app.service'
-import { MongooseModule } from '@nestjs/mongoose'
 import { UsersController } from './controllers/users.controller'
 import { User, UserSchema } from '@app/models/users/user.schema'
 import { AuthController } from './controllers/auth.controller'
@@ -10,7 +11,10 @@ import { AuthModule } from '@app/auth'
 
 @Module({
     imports: [
-        MongooseModule.forRoot('mongodb+srv://mdore:FjfrjcxKYfHowB0h@my-forum.48ponwu.mongodb.net/?retryWrites=true&w=majority', { dbName: 'my-forum' }),
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        MongooseModule.forRoot(process.env.MONGO_URI, { dbName: 'my-forum' }),
         MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
         UsersModule,
         AuthModule,
