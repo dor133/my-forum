a9229f3487e417628bd895a4b0064b30c18cd18f
dor133 <dore.mathieu@protonmail.com>
2023-04-13 16:46:51 +0200

    feat: start passport auth module

--- backend/libs/auth/src/auth.service.ts
+++ backend/libs/auth/src/auth.service.ts
@@ -1,7 +1,29 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { UsersService } from '@app/users'
+import { Injectable, UnauthorizedException, NotFoundException } from '@nestjs/common'
+// import { JwtService } from '@nestjs/jwt'
 import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class AuthService {
-    constructor() {}
+    constructor(private usersService: UsersService) {}
+
+    // async SignIn(username: string, password: string): Promise<any> {
+    //     const user = await this.usersService.findOne(username)
+    //     console.log(user)
+    //     const isMatch = await bcrypt.compare(password, user.password)
+    //     if (!isMatch) {
+    //         throw new UnauthorizedException('Invalid credentials')
+    //     }
+    // const payload = { username: user.username, sub: user._id }
+    // }
+
+    async validateUser(username: string, password: string): Promise<any> {
+        const user = await this.usersService.findOne(username)
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (user && isMatch) {
+            const { password, ...result } = user
+            return result
+        }
+        return null
+    }
 }
