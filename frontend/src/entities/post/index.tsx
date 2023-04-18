c46a7e2ec3b9f0be77cb080f02b8a400c26ee7c7
dor133 <dore.mathieu@protonmail.com>
2023-04-18 09:29:56 +0200

    refactor: print creation date in good format

--- frontend/src/entities/post/index.tsx
+++ frontend/src/entities/post/index.tsx
@@ -7,13 +7,17 @@ import { Stack } from '../../core/Stack'
 export function Post() {
     const { id } = useParams()
     const { data } = useGetPostQuery(id!)
-    console.log(typeof data?.createdDate)
+    // console.log(typeof data?.createdDate)
+    const date = new Date(data?.createdDate!)
+    // console.log(typeof date)
+    // console.log(typeof date.toDateString())
+
     return (
         <Card>
             <Stack spacing={4}>
                 <Text variant="title">{data?.title}</Text>
                 <Text variant="paragraph">
-                    Par: {data?.authorId} le {data?.createdDate.toString()}
+                    Par: {data?.authorId} le {date.toLocaleDateString()}
                 </Text>
                 <Text variant="paragraph">{data?.text}</Text>
             </Stack>
