0a32fafcf9c07ab08caad727468a63360f0b38cf
dor133 <dore.mathieu@protonmail.com>
2023-04-20 17:31:35 +0200

    refactor: moove protectedRoute component to entities

---
+++ frontend/src/entities/security/protectedRoute.tsx
@@ -0,0 +1,18 @@
+import { Navigate, useLocation } from 'react-router-dom'
+import useAuthStore from '../../store/auth/auth.store'
+import { ReactNode } from 'react'
+
+type Props = {
+    children: JSX.Element
+}
+
+export function ProtectedRoute({ children }: Props) {
+    const { payload } = useAuthStore()
+    let location = useLocation()
+
+    if (!payload) {
+        return <Navigate to="/login" state={{ from: location }} replace />
+    }
+
+    return children
+}
