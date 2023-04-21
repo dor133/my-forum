2babbb28e90300a82c3b01d67e38178993f8f6b3
dor133 <dore.mathieu@protonmail.com>
2023-04-21 10:51:46 +0200

    style: add red version

--- frontend/src/core/Button/index.tsx
+++ frontend/src/core/Button/index.tsx
@@ -1,10 +1,12 @@
 import cx from 'classnames'
 
 type Sizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+type Colors = 'green' | 'red'
 
 type Props = JSX.IntrinsicElements['button'] & {
     size?: Sizes
     active?: boolean
+    color?: Colors
 }
 
 const sizes: Record<Sizes, string> = {
@@ -15,14 +17,19 @@ const sizes: Record<Sizes, string> = {
     xl: 'px-3.5 py-2.5 text-sm',
 }
 
-export function Button({ size = 'md', children, className, active = true, ...rest }: Props) {
+export function Button({ size = 'md', color = 'green', children, className, active = true, ...rest }: Props) {
     return (
         <button
             type="button"
             className={cx(
-                active
+                color == 'red'
+                    ? active
+                        ? 'rounded bg-red-600 font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600'
+                        : 'rounded bg-gray-400 font-semibold text-gray-700 shadow-sm'
+                    : active
                     ? 'rounded bg-primary-600 font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600'
                     : 'rounded bg-gray-400 font-semibold text-gray-700 shadow-sm',
+
                 sizes[size],
                 className
             )}
