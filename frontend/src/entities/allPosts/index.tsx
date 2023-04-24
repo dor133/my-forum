7766762129534dc978e774eaff7a3615a2f0bb02
dor133 <dore.mathieu@protonmail.com>
2023-04-24 14:47:29 +0200

    feat: memoization of search

--- frontend/src/entities/allPosts/index.tsx
+++ frontend/src/entities/allPosts/index.tsx
@@ -6,18 +6,40 @@ import useAuthStore from '../../store/auth/auth.store'
 import { useGetPostsQuery } from '../../store/rtk/posts'
 import { Center } from '../../core/Center'
 import { Input } from '../../core/Input'
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import { Link } from 'react-router-dom'
+import { debounce } from 'lodash'
 
 export function AllPosts() {
     const { data } = useGetPostsQuery()
     const { payload } = useAuthStore()
 
+    const debouncedSearch = debounce(async (searchTerm: string) => {
+        setSearchTerm(searchTerm)
+    }, 500)
+
     const [searchTerm, setSearchTerm] = useState('')
     const handleSearchTerm = (e: React.ChangeEvent<HTMLInputElement>) => {
-        let value = e.target.value
-        setSearchTerm(value)
+        debouncedSearch(e.target.value)
     }
+
+    const filteredPosts = useMemo(
+        () =>
+            data?.filter((post) => {
+                console.log('filtering users')
+                return post.title
+                    .toLowerCase()
+                    .normalize('NFD')
+                    .replace(/\p{Diacritic}/gu, '')
+                    .includes(
+                        searchTerm
+                            .toLowerCase()
+                            .normalize('NFD')
+                            .replace(/\p{Diacritic}/gu, '')
+                    )
+            }),
+        [searchTerm]
+    )
 
     return (
         <>
@@ -37,24 +59,11 @@ export function AllPosts() {
 
                 <Text variant="paragraph">
                     <ul className="list-disc list-inside">
-                        {data
-                            ?.filter((post) =>
-                                post.title
-                                    .toLowerCase()
-                                    .normalize('NFD')
-                                    .replace(/\p{Diacritic}/gu, '')
-                                    .includes(
-                                        searchTerm
-                                            .toLowerCase()
-                                            .normalize('NFD')
-                                            .replace(/\p{Diacritic}/gu, '')
-                                    )
-                            )
-                            .map((post) => (
-                                <li key={post._id}>
-                                    <Link to={`/posts/${post._id}`}>{post.title}</Link>
-                                </li>
-                            ))}
+                        {filteredPosts?.map((post) => (
+                            <li key={post._id}>
+                                <Link to={`/posts/${post._id}`}>{post.title}</Link>
+                            </li>
+                        ))}
                     </ul>
                 </Text>
             </Stack>
