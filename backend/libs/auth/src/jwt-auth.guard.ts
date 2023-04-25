4a4a3924fe0fadf4ef03309c12c0545149f750bd
dor133 <dore.mathieu@protonmail.com>
2023-04-25 17:23:26 +0200

    feat: modify guard to have the user object on public routes

--- backend/libs/auth/src/jwt-auth.guard.ts
+++ backend/libs/auth/src/jwt-auth.guard.ts
@@ -2,6 +2,12 @@ import { Injectable, ExecutionContext } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { Reflector } from '@nestjs/core'
 import { IS_PUBLIC_KEY } from './jwt-public'
+import decode from 'jwt-decode'
+
+type JWTPayload = {
+    username: string
+    sub: string
+}
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
@@ -12,6 +18,13 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     canActivate(context: ExecutionContext) {
         const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [context.getHandler(), context.getClass()])
         if (isPublic) {
+            const request = context.switchToHttp().getRequest()
+            const authorization = request.headers.authorization
+            if (authorization && authorization.split(' ')[0] === 'Bearer') {
+                const token = authorization.split(' ')[1]
+                const payload = decode<JWTPayload>(token)
+                request.user = { username: payload.username, userId: payload.sub }
+            }
             return true
         }
         return super.canActivate(context)
