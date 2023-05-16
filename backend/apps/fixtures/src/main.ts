327e112fbd60db8d0277310ccef1f08116976867
dor133 <dore.mathieu@protonmail.com>
2023-05-16 16:26:38 +0200

    fix: try connecting to mongo cluster

--- backend/apps/fixtures/src/main.ts
+++ backend/apps/fixtures/src/main.ts
@@ -1,11 +1,18 @@
 import { CommandFactory } from 'nest-commander'
 import { FixturesModule } from './fixtures.module'
+import { connect } from 'mongoose'
 
 async function bootstrap() {
-    const start_time = Date.now()
-    console.log('Creating database ' + process.env.MONGO_RANDOM_ID)
-    await CommandFactory.run(FixturesModule)
-    console.log('Database created')
-    console.log('Duration: ' + (Date.now() - start_time) / 1000 + 's')
+    console.log('test: try connect to mongo')
+    const uri = process.env.MONGO_URI + '/' + process.env.MONGO_RANDOM_ID
+    console.log(uri)
+    const db = (await connect(uri)).connection
+    console.log('test: connected to mongo')
+
+    // const start_time = Date.now()
+    // console.log('Creating database ' + process.env.MONGO_RANDOM_ID)
+    // await CommandFactory.run(FixturesModule)
+    // console.log('Database created')
+    // console.log('Duration: ' + (Date.now() - start_time) / 1000 + 's')
 }
 bootstrap()
